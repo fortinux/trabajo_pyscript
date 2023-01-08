9f22071f4369147f8d76b607c162ac0dd255abba
fortinux <info@fortinux.com>
2023-01-08 11:21:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# trabajo_ejemplo
+[![Build Status](http://10.56.250.46:8080/buildStatus/icon?job=python_script&build=40)](http://10.56.250.46:8080/job/python_script/40/)
+  
+- Ejecuta un script de python en jenkins
+
---
+++ pyscript.py
@@ -0,0 +1,17 @@
+#!/user/bin/env python3
+"""
+[Add module documentation here]
+
+Author: Fortinux
+info@fortinux.com
+Date: [Add date here]
+"""
+
+
+from datetime import datetime
+import sys
+
+
+if __name__ == '__main__':
+    print(sys.executable)
+    print("Este script se ha ejecutado el " + str(datetime.now()))
